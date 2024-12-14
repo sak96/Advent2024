f06ddca8a1c7406749074ece605af4ca2bd1e7b3
sak96 <26397224+sak96@users.noreply.github.com>
2024-12-14 22:26:10 +0530

    Pre calculate day 06

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -1,6 +1,6 @@
 use std::io::BufRead;
 
-use fxhash::FxHashSet;
+use fxhash::{FxHashMap, FxHashSet};
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash)]
 enum Direction {
@@ -20,75 +20,74 @@ impl Direction {
         }
     }
 
-    pub fn turn(&mut self) {
-        *self = match self {
+    pub fn turn(&self) -> Self {
+        match self {
             Direction::N => Direction::E,
             Direction::S => Direction::W,
             Direction::W => Direction::N,
             Direction::E => Direction::S,
-        };
+        }
     }
 }
 
 pub fn run<'a>(reader: Box<dyn BufRead + 'a>) {
     let (mut sx, mut sy) = (0, 0);
-    let mut obstacles = FxHashSet::default();
+    let mut memory = FxHashMap::default();
 
-    let (mut row, mut col) = (0, 0);
-    for (r, line) in reader.lines().map_while(Result::ok).enumerate() {
-        for (c, &ch) in line.as_bytes().iter().enumerate() {
-            if ch == b'#' {
-                obstacles.insert((r as isize, c as isize));
-            } else if ch == b'^' {
-                (sx, sy) = (r, c);
+    let map: Vec<_> = reader
+        .lines()
+        .map_while(Result::ok)
+        .map(|line| line.as_bytes().to_vec())
+        .collect();
+
+    for (r, row) in map.iter().enumerate() {
+        for (c, &ch) in row.iter().enumerate() {
+            for d in [Direction::N, Direction::E, Direction::W, Direction::S] {
+                if ch == b'#' {
+                    continue;
+                } else if ch == b'^' {
+                    (sx, sy) = (r, c);
+                }
+                let r = r as isize;
+                let c = c as isize;
+                let (x, y) = d.move_();
+                let (x, y) = (r + x, c + y);
+                if x < 0 || y < 0 || x as usize >= map.len() || y as usize >= map[x as usize].len()
+                {
+                    memory.insert((r, c, d), None);
+                } else if map[x as usize][y as usize] == b'#' {
+                    memory.insert((r, c, d), Some((r, c, d.turn())));
+                } else {
+                    memory.insert((r, c, d), Some((x, y, d)));
+                }
             }
         }
-        col = line.len();
-        row = row.max(r + 1);
     }
 
-    let (mut i, mut j) = (sx as isize, sy as isize);
-    let mut direction = Direction::N;
+    let (mut x, mut y, mut d) = (sx as isize, sy as isize, Direction::N);
     let mut moves = FxHashSet::default();
-    loop {
-        if i < 0 || j < 0 || i as usize > row || j as usize > col {
-            println!("{}", moves.len());
-            break;
-        }
-        moves.insert((i, j));
-        let (x, y) = direction.move_();
-        if obstacles.contains(&(i + x, j + y)) {
-            direction.turn();
-        } else {
-            i += x;
-            j += y;
-        }
+    moves.insert((x, y));
+    while let Some((x1, y1, d1)) = memory.get(&(x, y, d)).unwrap() {
+        (x, y, d) = (*x1, *y1, *d1);
+        moves.insert((x, y));
     }
+    println!("{}", moves.len());
 
     let mut count = 0;
-    moves.remove(&(sx as _, sy as _));
     for (r, c) in moves {
-        obstacles.insert((r, c));
+        let (mut x, mut y, mut d) = (sx as isize, sy as isize, Direction::N);
         let mut moves = FxHashSet::default();
-        let (mut i, mut j) = (sx as isize, sy as isize);
-        let mut direction = Direction::N;
-        loop {
-            if i < 0 || j < 0 || i as usize > row || j as usize > col {
-                break;
+        while let Some((x1, y1, d1)) = memory.get(&(x, y, d)).unwrap() {
+            if (x1, y1) == (&r, &c) {
+                d = d.turn();
+            } else {
+                (x, y, d) = (*x1, *y1, *d1);
             }
-            if !moves.insert((i, j, direction)) {
+            if !moves.insert((x, y, d)) {
                 count += 1;
                 break;
             }
-            let (x, y) = direction.move_();
-            if obstacles.contains(&(i + x, j + y)) {
-                direction.turn();
-            } else {
-                i += x;
-                j += y;
-            }
         }
-        obstacles.remove(&(r, c));
     }
     println!("{count}");
 }
